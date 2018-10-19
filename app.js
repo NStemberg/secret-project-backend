446d2ce5633f2b11b8ed9fcfb7d2830ce82f03cb
NStemberg <niklas.stemberg@de.ibm.com>
2018-10-19 18:13:49 +0200

    new Commit

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+    res.json(JSON.stringify({name: 'Hello World!'}));
+});
+
+app.get('/test', function (res, res) {
     res.send('Hello World!');
 });
 
 // use port 3000 unless there exists a preconfigured port
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port);
