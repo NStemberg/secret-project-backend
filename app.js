87edb9408eeb27706e93bea4d183d8b1537cd840
NStemberg <niklas.stemberg@de.ibm.com>
2018-12-03 17:41:39 +0100

    Updated Backend

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var EventEmitter = require('events');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
 var players = {};
+var x = 0;
+var host = '';
+
+const emitter = new EventEmitter();
 
 app.get('/', function (req, res) {
     res.json(JSON.stringify({name: 'Hello World!'}));
@@ -20,9 +25,20 @@ io.on('connection', function (socket) {
     console.log('a user connected');
     players[socket.id] = {
         x: 100,
-        y: 450,
+        y: 350,
+        score: 0,
         playerId: socket.id,
     };
+    if(host === '') {
+        host = socket.id;
+    }
+    if(socket.id === host){
+        setInterval(() => {
+            const coord = (100 + (Math.random() * 300));
+            socket.emit('createObst', {y: coord});
+            socket.broadcast.emit('createObst', {y: coord});
+        }, 1000)
+    }
     socket.emit('currentPlayers', players);
     socket.broadcast.emit('newPlayer', players[socket.id]);
     console.log(players);
@@ -32,12 +48,17 @@ io.on('connection', function (socket) {
         io.emit('disconnect', socket.id);
 
     })
+    socket.on('collision', function() {
+        players[socket.id].score += 1;
+        socket.emit('collision', players);
+        socket.broadcast.emit('collision', players);
+    })
     socket.on('playerMovement', function (movementData) {
         players[socket.id].x = movementData.x;
         players[socket.id].y = movementData.y;
         // emit a message to all players about the player that moved
         socket.broadcast.emit('playerMoved', players[socket.id]);
-    });;
+    });
 });
 
 server.listen(port);
